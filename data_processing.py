75b4aa5e041de1cbc7914ee2b168680d390212b9
Abe <twistedtech.in@gmail.com>
2022-12-20 06:57:49 +0530

    added ml modelcode

--- data_processing.py
+++ data_processing.py
@@ -1,12 +1,2 @@
 
 print('Adding all the data processing code related to fraud detection')
-
-
-a = 5
-b = 7
-
-def sum(a,b):
-    c = a + b
-    return a
-
-print("added a new function to sum up two numbers which is not correct")
